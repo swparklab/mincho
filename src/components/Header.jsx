02db3edbdcf0212f838fe4284e501d48463dc69b
parksw <1998psw@korea.ac.kr>
2023-04-11 18:25:34 +0900

    123

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@ import minchologo from '../img/minchologo.png'
 
 function Header() {
   return (
-    <header className="bg-green-300 h-12 md:h-20">
+    <header className="bg-green-300 h-20">
       <div className="max-w-screen-xl mx-auto h-full flex justify-between items-center">
-        <div className="w-1/4 mx-4">
-        <img src={minchologo} alt="minchologo" style={{ width: '100px', height: 'auto' }} />
+        <div className="w-1/4 mx-4 md:w-1/8">
+        <img src={minchologo} alt="minchologo" style={{ width: '80px', height: 'auto' }} />
         </div>
-        <ul className="flex text-xl font-medium md:text-base gap-2 md:gap-8 mx-8">
+        <ul className="flex text-xl font-medium gap-2 md:gap-8 mx-8 outline-white	">
           <li>Brain</li>
           <li>CELL</li>
           <li>WASTE</li>
